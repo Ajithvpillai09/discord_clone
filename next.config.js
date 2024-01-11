ea873425b1899906b9d072c7ca3c948e9fa52bac
Ajithvpillai09 <ajithvkallada@gmail.com>
2024-01-11 16:28:29 +0530

    completed modal and server creation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:["uploathing.com","utfs.io"]
+    }
+}
 
 module.exports = nextConfig
